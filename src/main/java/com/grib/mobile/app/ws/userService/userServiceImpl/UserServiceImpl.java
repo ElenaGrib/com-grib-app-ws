267556cf16d8929de458a04fa7da7582f3e0c632
ElenaGrib <elenagrib27@gmail.com>
2022-08-13 18:01:30 +0300

    7. Spring - DI

--- src/main/java/com/grib/mobile/app/ws/userService/userServiceImpl/UserServiceImpl.java
+++ src/main/java/com/grib/mobile/app/ws/userService/userServiceImpl/UserServiceImpl.java
@@ -1,8 +1,10 @@
 package com.grib.mobile.app.ws.userService.userServiceImpl;
 
+import com.grib.mobile.app.ws.shared.Utils;
 import com.grib.mobile.app.ws.ui.model.request.UserDetailsRequestModel;
 import com.grib.mobile.app.ws.ui.model.responce.UserRest;
 import com.grib.mobile.app.ws.userService.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
@@ -11,7 +13,16 @@ import java.util.UUID;
 
 @Service
 public class UserServiceImpl implements UserService {
+    Utils utils;
     Map<String, UserRest> users;
+
+    public UserServiceImpl() {
+    }
+
+    @Autowired
+    public UserServiceImpl(Utils utils) {
+        this.utils = utils;
+    }
 
     @Override
     public UserRest createUser(UserDetailsRequestModel userDetails) {
@@ -20,7 +31,7 @@ public class UserServiceImpl implements UserService {
         returnUser.setLastName(userDetails.getLastName());
         returnUser.setEmail(userDetails.getEmail());
 
-        String userId = UUID.randomUUID().toString();
+        String userId = utils.generateUserId();
         returnUser.setUserId(userId);
 
         if (users == null) users = new HashMap<>();
